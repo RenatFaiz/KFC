2927073319c25eab99fe87d717d6a8848d7daed0
RenatFaiz <renatf87@gmail.com>
2019-10-15 15:39:01 +0400

    New fields added

--- src/main/java/kfc/Burger.java
+++ src/main/java/kfc/Burger.java
@@ -1,12 +1,19 @@
 package kfc;
 
 public class Burger {
+    int id;
     String name;
     int price;
+    int weight;
+    int calories;
+    String link;
     String ingredients;
 
     void print() {
         System.out.println(name + ", " + price + " RUB");
         System.out.println("Ingredients: " + ingredients);
+        System.out.println("Weight and calories: " + weight +
+                "g, " + calories + "ccal");
+        System.out.println("Click here to order: " + link + "\n");
     }
 }
--- src/main/java/kfc/Main.java
+++ src/main/java/kfc/Main.java
@@ -4,14 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
         Burger cheeseBurger = new Burger();
-        cheeseBurger.name = "CheeseBurger";
+        cheeseBurger.id = 2336;
+        cheeseBurger.name = "Cheeseburger";
         cheeseBurger.price = 69;
+        cheeseBurger.weight = 167;
+        cheeseBurger.calories = 237;
+        cheeseBurger.link = "https://www.kfc.ru/en/product/2336";
         cheeseBurger.ingredients = "herby mustard sauce, ketchup, " +
-                "onion,\ncheddar cheese, pickles on a corn bun.\n";
+                "onion,\ncheddar cheese, pickles on a corn bun.";
 
         Burger chefBurger = new Burger();
+        chefBurger.id = 2384;
         chefBurger.name = "Chefburger";
         chefBurger.price = 129;
+        chefBurger.weight = 213;
+        chefBurger.calories = 236;
+        chefBurger.link = "https://www.kfc.ru/en/product/2384";
         chefBurger.ingredients = "juicy fillet in original breading, lettuce and Tomatoeses\n" +
                 "on a wheat bun with black and white sesame seeds.";
 
